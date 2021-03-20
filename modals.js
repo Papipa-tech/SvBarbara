3d3fc519f0372aa61d7886562a3390fdddcb1426
Filip Franicevic <fliksauskas22@gmail.com>
2021-03-20 23:05:41 +0100

    modali klasiranje, usitnjavanje

--- modals.js
+++ modals.js
@@ -1,58 +1,95 @@
 "use-strict";
-// Grabbing universal modal elements
-const mainModal = document.querySelector(".modal-window");
-const mainOverlay = document.querySelector(".modal-overlay");
-const mainCloseModal = document.querySelector(".close-modal");
+// KLASIRANJE////////////////////////////////////////
+// Grabbing modal elements
+//const mainModal = document.querySelector('.modal-usitnjavanje');
+const mainModalKlasiranje = document.querySelector(".modal-klasiranje");
+// generic modal properties
+const mainOverlayKlasiranje = document.querySelector(".modal-overlay");
+const mainCloseModalKlasiranje = document.querySelector(".close-modal");
 
 // Grabbing custom modal elements
 // Kamenolomi
-const modalUsitnjavanje = document.getElementById("usitnjavanje");
+// const modalUsitnjavanje = document.getElementById("usitnjavanje");
 const modalKlasiranje = document.getElementById("klasiranje");
-const modalTransport = document.getElementById("transport");
-const modalOtprasivanje = document.getElementById("otprasivanje");
-const modalMiniranje = document.getElementById("miniranje");
-const modalPogon = document.getElementById("pogon");
-const modalUtovar = document.getElementById("utovar");
-const modalSigurnost = document.getElementById("sigurnost");
+// const modalTransport = document.getElementById("transport");
+// const modalOtprasivanje = document.getElementById("otprasivanje");
+// const modalMiniranje = document.getElementById("miniranje");
+// const modalPogon = document.getElementById("pogon");
+// const modalUtovar = document.getElementById("utovar");
+// const modalSigurnost = document.getElementById("sigurnost");
 // Reciklaza
-const modalGradevinskiMaterijal = document.getElementById(
-  "gradevinskiMaterijal"
-);
-const modalDrvo = document.getElementById("drvo");
-const modalMetal = document.getElementById("metal");
-const modalBioOtpad = document.getElementById("bioOtpad");
-const modalOstali = document.getElementById("ostali");
+// const modalGradevinskiMaterijal = document.getElementById(
+//   "gradevinskiMaterijal"
+// );
+// const modalDrvo = document.getElementById("drvo");
+// const modalMetal = document.getElementById("metal");
+// const modalBioOtpad = document.getElementById("bioOtpad");
+// const modalOstali = document.getElementById("ostali");
 
-const openModal = function () {
-  mainModal.classList.remove("hidden");
-  mainOverlay.classList.remove("hidden");
+const openModalKlasiranje = function () {
+  mainModalKlasiranje.classList.remove("hidden");
+  mainOverlayKlasiranje.classList.remove("hidden");
 };
 
-const closeModal = function () {
-  mainModal.classList.add("hidden");
-  mainOverlay.classList.add("hidden");
+const closeModalKlasiranje = function () {
+  mainModalKlasiranje.classList.add("hidden");
+  mainOverlayKlasiranje.classList.add("hidden");
 };
 
-mainCloseModal.addEventListener("click", closeModal);
-mainOverlay.addEventListener("click", closeModal);
+mainCloseModalKlasiranje.addEventListener("click", closeModalKlasiranje);
+mainOverlayKlasiranje.addEventListener("click", closeModalKlasiranje);
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !mainModal.classList.contains("hidden")) {
-    closeModal();
+  if (e.key === "Escape" && !mainModalKlasiranje.classList.contains("hidden")) {
+    closeModalKlasiranje();
   }
 });
 
 // Custom modals
-modalUsitnjavanje.addEventListener("click", openModal);
-modalKlasiranje.addEventListener("click", openModal);
-modalTransport.addEventListener("click", openModal);
-modalOtprasivanje.addEventListener("click", openModal);
-modalMiniranje.addEventListener("click", openModal);
-modalPogon.addEventListener("click", openModal);
-modalUtovar.addEventListener("click", openModal);
-modalSigurnost.addEventListener("click", openModal);
-modalGradevinskiMaterijal.addEventListener("click", openModal);
-modalDrvo.addEventListener("click", openModal);
-modalMetal.addEventListener("click", openModal);
-modalBioOtpad.addEventListener("click", openModal);
-modalOstali.addEventListener("click", openModal);
+//modalUsitnjavanje.addEventListener("click", openModalUsitnjavanje);
+modalKlasiranje.addEventListener("click", openModalKlasiranje);
+// modalTransport.addEventListener("click", openModal);
+// modalOtprasivanje.addEventListener("click", openModal);
+// modalMiniranje.addEventListener("click", openModal);
+// modalPogon.addEventListener("click", openModal);
+// modalUtovar.addEventListener("click", openModal);
+// modalSigurnost.addEventListener("click", openModal);
+// modalGradevinskiMaterijal.addEventListener("click", openModal);
+// modalDrvo.addEventListener("click", openModal);
+// modalMetal.addEventListener("click", openModal);
+// modalBioOtpad.addEventListener("click", openModal);
+// modalOstali.addEventListener("click", openModal);
+
+//USITNJAVANJE
+const mainModalUsitnjavanje = document.querySelector(".modal-usitnjavanje");
+// generic modal properties
+const mainOverlayUsitnjavanje = document.querySelector(".modal-overlay");
+const mainCloseModalUsitnjavanje = document.querySelector(
+  ".close-usitnjavanje"
+);
+
+const modalUsitnjavanje = document.getElementById("usitnjavanje");
+
+const openModalUsitnjavanje = function () {
+  mainModalUsitnjavanje.classList.remove("hidden");
+  mainOverlayUsitnjavanje.classList.remove("hidden");
+};
+
+const closeModalUsitnjavanje = function () {
+  mainModalUsitnjavanje.classList.add("hidden");
+  mainOverlayUsitnjavanje.classList.add("hidden");
+};
+
+mainCloseModalUsitnjavanje.addEventListener("click", closeModalUsitnjavanje);
+mainOverlayUsitnjavanje.addEventListener("click", closeModalUsitnjavanje);
+
+document.addEventListener("keydown", function (e) {
+  if (
+    e.key === "Escape" &&
+    !mainModalUsitnjavanje.classList.contains("hidden")
+  ) {
+    closeModalUsitnjavanje();
+  }
+});
+
+modalUsitnjavanje.addEventListener("click", openModalUsitnjavanje);
